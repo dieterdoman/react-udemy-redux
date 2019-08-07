7cd1bbd9a57fce6cafe5783d2ecb97189ffba00e
Dieter Doman <dieter@jemstep.com>
2019-08-07 15:56:23 +0200

    Added stored results to redux state.

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    render () {
+    render() {
         return (
             <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <CounterOutput value={this.props.ctr}/>
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter}/>
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}/>
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}/>
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}/>
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storeResults.map(storeResult => <li key={storeResult.id} onClick={this.props.onDeleteResult}>{storeResult.value}</li>)}
+                </ul>
             </div>
         );
     }
@@ -19,7 +24,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-          ctr: state.counter
+        ctr: state.counter,
+        storeResults: state.results
     };
 };
 
@@ -28,7 +34,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', value: 5}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 5})
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     };
 };
 
