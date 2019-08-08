d0651bb8d419cab7dbe4bdc69d62c046ba50dd35
Dieter Doman <dieter@jemstep.com>
2019-08-08 15:54:25 +0200

    Added utility function to reducers to immutably update objects.

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionsTypes';
+import updateObject from "../utility";
 
 const initialState = {
     results: []
@@ -7,16 +8,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({value: action.result, id: new Date()})
-            };
+            return updateObject(state,{results: state.results.concat({value: action.result, id: new Date()})});
         case actionTypes.DELETE_RESULT:
             const updatedArray = state.results.filter(element => element.id !== action.resultId);
-            return {
-                ...state,
-                results: updatedArray
-            };
+            return updateObject(state,{results: updatedArray});
         default:
             return state;
     }
