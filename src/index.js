dc5bdd0165c5846bc990b8eb3fb684804d4764be
Dieter Doman <dieter@jemstep.com>
2019-08-08 14:37:10 +0200

    Added dev tools for redux.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultsReducer from './store/reducers/results';
 import {Provider} from 'react-redux';
@@ -13,7 +13,19 @@ const rootReducer = combineReducers({
    results: resultsReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('middleware dispatching', action);
+            const result = next(action);
+            console.log('next state', store.getState());
+            return result;
+        };
+    };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
