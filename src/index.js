2e3405eac32c9dac7a61e663ab0e2a5589704c99
Dieter Doman <dieter@jemstep.com>
2019-08-08 15:08:49 +0200

    Added redux thunk and added async actions.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultsReducer from './store/reducers/results';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
    counter: counterReducer,
@@ -25,7 +26,7 @@ const logger = store => {
 };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
