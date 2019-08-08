6a09db459cfd6bcbb7d6d23154ce98c5e48574bd
Dieter Doman <dieter@jemstep.com>
2019-08-08 15:57:37 +0200

    Added small helper function to clarify intent.

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -5,13 +5,17 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(element => element.id !== action.resultId);
+    return updateObject(state,{results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             return updateObject(state,{results: state.results.concat({value: action.result, id: new Date()})});
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(element => element.id !== action.resultId);
-            return updateObject(state,{results: updatedArray});
+            return deleteResult(state, action);
         default:
             return state;
     }
